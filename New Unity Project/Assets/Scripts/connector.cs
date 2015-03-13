5afed11262c11c90bfd0394babdd64fdd55c63ee
DanilMonsur <danil.monsur1@gmail.com>
2015-03-12 22:08:37 -0700

    first

---
+++ New Unity Project/Assets/Scripts/connector.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class connector : MonoBehaviour {
+
+	public string connectionIp = "127.0.0.1";
+	public int connectionPort = 25001;
+
+	void OnGUI()
+	{
+		Application.runInBackground = true;
+
+		if (Network.peerType == NetworkPeerType.Disconnected) 
+		{
+			GUI.Label (new Rect(10, 10, 200, 20), "Status: Disconnected");
+
+			if (GUI.Button (new Rect(10, 30, 120, 20), "Client Connect"))
+			{
+				Network.Connect (connectionIp, connectionPort);
+			}
+
+			if (GUI.Button (new Rect(10, 50, 120, 20), "Initialize Server"))
+			{
+				Network.InitializeServer (3, connectionPort, false);
+			}
+		}
+		else if (Network.peerType == NetworkPeerType.Client)
+		{
+			GUI.Label (new Rect(10, 10, 300, 20), "Status: Connected as Client");
+
+			if (GUI.Button (new Rect(10, 30, 120, 20), "Disconnect"))
+			{
+				Network.Disconnect (200);
+			}
+		}
+		else if (Network.peerType == NetworkPeerType.Server)
+		{
+			GUI.Label (new Rect(10, 10, 300, 20), "Status: Connected as Server");
+
+			if (GUI.Button (new Rect(10, 30, 120, 20), "Disconnect"))
+			{
+				Network.Disconnect (200);
+			}
+		}
+	}
+
+}
---
+++ New Unity Project/Assets/Scripts/enemyHPWatcher.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class enemyHPWatcher : MonoBehaviour {
+
+	private GameObject enemyHP;
+	private GameObject enemy;
+
+	// Use this for initialization
+	[RuntimeInitializeOnLoadMethod]
+	void Start () {
+		enemyHP = GameObject.Find ("Overlay/EnemyHP");
+		enemy = GameObject.Find ("Overlay/Enemy");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			int val = (int) enemyHP.GetComponent<Slider>().value;
+			stream.Serialize (ref val);
+		}
+		else
+		{
+			int receivedVal = 0;
+			stream.Serialize (ref receivedVal);
+			enemyHP.GetComponent<Slider>().value = receivedVal;
+		}
+		
+	}
+}
